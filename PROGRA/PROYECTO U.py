5a85896592df89a8135fdbae551da68d5bd02b8e
Alessandrito502 <samuellux088@gmail.com>
2026-05-18 16:46:02 -0600

    Update PROYECTO U.py

--- PROGRA/PROYECTO U.py
+++ PROGRA/PROYECTO U.py
@@ -341,5 +341,5 @@ def config():
     return base("Configuración", form, "#ff9f0a")
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5001)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
